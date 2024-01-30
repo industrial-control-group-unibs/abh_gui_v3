b65113971722ee8cf5d09558eb2f15b7b2bbbd2f
Manuel Beschi <manuel.beschi@unibs.it>
2024-01-30 09:31:13 +0100

    add template

--- template/script_update.sh
+++ template/script_update.sh
@@ -10,11 +10,11 @@ git pull origin main
 
 
 cp ~/Scrivania/abh/abh_data/traduzioni/*.ts ~/projects/abh_gui_v3
-lupdate -extensions qml -ts abh_ar.ts
-lupdate -extensions qml -ts abh_de.ts
-lupdate -extensions qml -ts abh_en.ts
-lupdate -extensions qml -ts abh_fr.ts
-lupdate -extensions qml -ts abh_it.ts
+#lupdate -extensions qml -ts abh_ar.ts
+#lupdate -extensions qml -ts abh_de.ts
+#lupdate -extensions qml -ts abh_en.ts
+#lupdate -extensions qml -ts abh_fr.ts
+#lupdate -extensions qml -ts abh_it.ts
 
 
 cd build
