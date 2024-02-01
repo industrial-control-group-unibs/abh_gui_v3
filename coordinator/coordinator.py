d1fe27f0c71a67b85e1f97db92a4ccde81358e5d
Manuel Beschi <manuel.beschi@unibs.it>
2024-02-01 17:38:14 +0100

    manuel

--- coordinator/coordinator.py
+++ coordinator/coordinator.py
@@ -315,6 +315,7 @@ def exercise_thread():
                     motor_target_data=[0,exercise["force"]/100,exercise["velocity"]/100,torque_change_time_bw]
                 if not isinstance(exercise_name_eval,int):
                     print("Start vision")
+                    exercise_name_eval.sendString(esercizio)
                     exercise_name_eval.sendString("start")
 
             elif stringa=="rewire":
