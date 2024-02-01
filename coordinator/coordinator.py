68a44d7c85d6c328ab0c985faab730c1b47344ab
Manuel Beschi <manuel.beschi@unibs.it>
2024-02-01 11:06:44 +0100

    foto

--- coordinator/coordinator.py
+++ coordinator/coordinator.py
@@ -349,7 +349,7 @@ def exercise_thread():
             calibrating = True
             force_power_level_0_first_reps=force_power_level_0_last_reps
             force_power_first_reps=force_power_last_reps
-            
+
 
         prev_motor_status = motor_status
         if motor_status == MotorStatus.REST_BACKWARD and motor_speed<motor_speed_threshold_return*2.0:
@@ -433,7 +433,7 @@ def exercise_thread():
             elif (state == Status.REWIRE):
                 motor_target_data=[0,0.20,0.2,1]
             motor_target.sendData(motor_target_data)
-            print(f"state = {state}")
+            print(f"state = {state}, motor = {motor_target_data}")
             last_state = state
 
     if not isinstance(exercise_client,type):
